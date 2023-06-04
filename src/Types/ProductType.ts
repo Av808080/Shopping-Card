70f9b442379bc2bb231846881b204e19d800a1c7
Av808080 <108837984+Av808080@users.noreply.github.com>
2023-06-04 19:44:12 +0330

    Annonce Product Type

---
+++ src/Types/ProductType.ts
@@ -0,0 +1,13 @@
+type Product = {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+};
+export default Product;
