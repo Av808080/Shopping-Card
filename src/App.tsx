10cb9cd60ccc34c36f4f25e893212113d9527974
Av808080 <108837984+Av808080@users.noreply.github.com>
2023-06-05 16:59:11 +0330

    Store JSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
+import Store from './Components/Store';
 //Context
 import ProductContextProvider from './context/ProductContextProvider';
 function App() {
   return (
-   <>
-   <ProductContextProvider>
-    
-   </ProductContextProvider>
-   
-   </>
+    <>
+      <ProductContextProvider>
+        <Store />
+      </ProductContextProvider>
+
+    </>
   );
 }
 
