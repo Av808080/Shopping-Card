3740677813d10c1ed89f7f6501c23e26f569a12f
Av808080 <108837984+Av808080@users.noreply.github.com>
2023-06-08 23:32:25 +0330

    Not Found Page styles

--- src/Components/NotFound.tsx
+++ src/Components/NotFound.tsx
@@ -1,13 +1,19 @@
 import { Link } from 'react-router-dom';
+import Notfound from '../assets/PageNotFound.png'
+import styles from './Sass/NotFound.module.scss'
 
 const NotFound = () => {
     document.title = "Page Not Found"
     return (
-        <h3>
-            Page Not Found
-            <p> 404 Page</p>
-            <Link to='/products'>Back to Shop</Link>
-        </h3>
+        <>
+            <img className={styles.img} src={Notfound} alt="404" />
+            <div className={styles.text}>
+                <h3 className={styles.alert}>
+                    Page Not Found
+                </h3>
+                <Link className={styles.backLink} to='/products'>Back to Shop</Link>
+            </div>
+        </>
     )
 }
 
