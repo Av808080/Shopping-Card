5f426e54e7cf1fc07f5f82e93d177772fd973058
Av808080 <108837984+Av808080@users.noreply.github.com>
2023-06-09 23:49:38 +0330

    Navbar styles

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,12 +1,13 @@
 import { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { CartContext } from '../context/CartContextProvider'
+import styles from './Sass/Navbar.module.scss'
 const Navbar = () => {
   const { state } = useContext(CartContext);
   return (
-    <nav>
-      <Link to='/products'>Products</Link>
-      <Link to='/cart'>Cart <span>{state.itemsCounter}</span></Link>
+    <nav className={styles.navbar}>
+      <Link className={styles.link} to='/products'>Products</Link>
+      <Link className={styles.link}to='/cart'>Cart <span>{state.itemsCounter}</span></Link>
     </nav>
   )
 }
